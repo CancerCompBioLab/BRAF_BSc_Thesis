880da3abfc44b4f205046171f738b88d1045f967
Taras Yuziv Duda <144703808+tarasyuzivduda@users.noreply.github.com>
2025-06-17 15:39:40 +0200

    Update 0_TCGAbiolinks.R

--- Scripts/0_TCGAbiolinks.R
+++ Scripts/0_TCGAbiolinks.R
@@ -13,10 +13,9 @@ TCGA.COAD.exp <- GDCprepare(query_exp)
 
 #Raw counts are in unstranded
 exp_COAD_raw<-assays(TCGA.COAD.exp)$unstranded
-#Let's select the TPM counts
+#TPM counts
 exp_COAD<-assays(TCGA.COAD.exp)$tpm_unstrand
-#Convert to dataframe
-exp_COAD<-as.data.frame(exp_COAD)
+
 
 
 # Query available files for TCGA-COAD
@@ -29,9 +28,5 @@ GDCdownload(query_mut)
 output_query_mutation<-getResults(query_mut)
 #Create a SummarizedExperiment
 mut_COAD<-GDCprepare(query_mut,summarizedExperiment = TRUE)
-#Shorten the sample name so that we can combine it will clinical_data file
-mut_COAD$Tumor_Sample_Barcode<-substr(mut_READ$Tumor_Sample_Barcode, 1, 12)
 #Create a MAF file
-# A MAF file is a tab-delimited text format designed specifically to store somatic mutation data for cancer genomics.
-#It is a standardized format used by The Cancer Genome Atlas (TCGA).
 maf <- read.maf(maf = mut_READ)
