d40abd929c148349683167d92f787b873ec0512d
Taras Yuziv Duda <144703808+tarasyuzivduda@users.noreply.github.com>
2025-06-17 10:50:12 +0200

    Update run_ARACNe_consolidate.sh

--- Scripts/ARACNe/run_ARACNe_consolidate.sh
+++ Scripts/ARACNe/run_ARACNe_consolidate.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-Aracne=./ARACNe-AP/Aracne.jar
+Aracne=./PATH/ARACNE-AP/file
 
 ls -l 
 [ -f "$Aracne" ] || { echo "ARACNe-AP Path $Aracne doesn't exist"; exit 1; } 
 echo "ARACNe-AP Path:$Aracne"
 echo "Calculating Threshold"
 
-java -Xmx16G -jar $Aracne -o Aracne_COAD_output_TCGA_protein_coding --consolidate
+java -Xmx16G -jar $Aracne -o output_folder --consolidate
