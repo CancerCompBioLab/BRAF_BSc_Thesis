f4b14d7d64c6a0e7ca6ec261b98e877ecd64d4f9
Taras Yuziv Duda <144703808+tarasyuzivduda@users.noreply.github.com>
2025-06-17 16:01:10 +0200

    Update 0_TCGAbiolinks.R

--- Scripts/0_TCGAbiolinks.R
+++ Scripts/0_TCGAbiolinks.R
@@ -13,9 +13,8 @@ TCGA.COAD.exp <- GDCprepare(query_exp)
 
 #Raw counts are in unstranded
 exp_COAD_raw<-assays(TCGA.COAD.exp)$unstranded
-#TPM counts
-exp_COAD<-assays(TCGA.COAD.exp)$tpm_unstrand
-
+# TPM counts
+exp_COAD_TPM<-assays(TCGA.COAD.exp)$tpm_unstrand
 
 
 # Query available files for TCGA-COAD
@@ -26,7 +25,11 @@ query_mut <- GDCquery(
 )
 GDCdownload(query_mut)
 output_query_mutation<-getResults(query_mut)
+#theres suplicates, so lets filter out
+unique_mutation_files <- output_query_mutation[!duplicated(output_query_mutation$cases), ]
+
+query_mut$results[[1]] <- unique_mutation_files
 #Create a SummarizedExperiment
 mut_COAD<-GDCprepare(query_mut,summarizedExperiment = TRUE)
-#Create a MAF file
-maf <- read.maf(maf = mut_READ)
+
+## PLEASE KEEP THE mut_COAD and exp_COAD_raw objects in your R Enviroment
