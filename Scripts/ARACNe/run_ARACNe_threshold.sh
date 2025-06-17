44f3408ceb8a3e2b40a3d1be2278ec06da90cfad
Taras Yuziv Duda <144703808+tarasyuzivduda@users.noreply.github.com>
2025-06-17 10:50:57 +0200

    Update run_ARACNe_threshold.sh

--- Scripts/ARACNe/run_ARACNe_threshold.sh
+++ Scripts/ARACNe/run_ARACNe_threshold.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-Aracne=./ARACNe-AP/Aracne.jar
+Aracne=./path/ARACNe-AP/file
 
 ls -l 
 [ -f "$Aracne" ] || { echo "ARACNe-AP Path $Aracne doesn't exist"; exit 1; } 
 echo "ARACNe-AP Path:$Aracne"
 echo "Calculating Threshold"
 
-java -Xmx20G -jar $Aracne -e ./ARACNe_log2transformed_TPM_MTAB_782samples.tsv -o Aracne_COAD_output_MTAB_protein_coding -t ./TF_COAD.txt --pvalue 1E-8 \
+java -Xmx20G -jar $Aracne -e ./samples -o output_folder -t Scripts/ARACNe/TF_COAD.txt --pvalue 1E-8 \
 --seed 1 --calculateThreshold
